dbd1c307f0e873d4673cdc41b7064fd7dad36ca2
footballfrenzy <90499274+footballfrenzy@users.noreply.github.com>
2022-05-03 03:20:16 +0200

    ahmed ibrahim

---
+++ js/clubs.js
@@ -0,0 +1,179 @@
+function myfunction1()
+{
+    var x = document.getElementById("star1");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction2()
+{
+    var x = document.getElementById("star2");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction3()
+{
+    var x = document.getElementById("star3");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction4()
+{
+    var x = document.getElementById("star4");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction5()
+{
+    var x = document.getElementById("star5");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction6()
+{
+    var x = document.getElementById("star6");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction7()
+{
+    var x = document.getElementById("star7");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png";
+}
+
+function myfunction8()
+{
+    var x = document.getElementById("star8");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction9()
+{
+    var x = document.getElementById("star9");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction10()
+{
+    var x = document.getElementById("star10");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction11()
+{
+    var x = document.getElementById("star11");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction12()
+{
+    var x = document.getElementById("star12");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png";
+}
+
+function myfunction13()
+{
+    var x = document.getElementById("star13");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction14()
+{
+    var x = document.getElementById("star14");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png";
+}
+
+function myfunction15()
+{
+    var x = document.getElementById("star15");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction16()
+{
+    var x = document.getElementById("star16");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction17()
+{
+    var x = document.getElementById("star17");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction18()
+{
+    var x = document.getElementById("star18");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/000000/star--v1.png";
+}
+
+function myfunction19()
+{
+    var x = document.getElementById("star19");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
+
+function myfunction20()
+{
+    var x = document.getElementById("star20");
+    if(x.src==="https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png")
+    x.src = "https://img.icons8.com/color/48/000000/filled-star--v1.png";
+    else
+    x.src = "https://img.icons8.com/fluency-systems-regular/48/ffffff/star--v1.png";
+}
